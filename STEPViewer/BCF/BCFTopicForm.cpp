6a8f117312bd9ff636df8ab811fdc1997485850d
I.Sokolov@yandex.ru <I.Sokolov@yandex.ru>
2026-09-11 14:05:17 +0300

    title && comment

--- STEPViewer/BCF/BCFTopicForm.cpp
+++ STEPViewer/BCF/BCFTopicForm.cpp
@@ -68,7 +68,7 @@ BOOL CBCFTopicForm::Create(CBCFView* pane)
 	m_tabs.Create(WS_CHILD | WS_VISIBLE | WS_TABSTOP | TCS_TABS, CRect(), this, IDC_PANE_TABS);	
 	SetBCFControlFont(m_tabs, this);
 	
-	m_tabs.InsertItem(0, L"Title & Comments");
+	m_tabs.InsertItem(0, L"Title && Comment");
 	m_tabs.InsertItem(1, L"Attributes");
 	m_tabs.InsertItem(2, L"BIM Files");
 	m_tabs.InsertItem(3, L"Snippet");
